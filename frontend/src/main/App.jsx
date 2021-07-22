7f9cf460cdd316cf33c963a6986e8568a53fb5c0
Rafael <rafaelsouzavalle@gmail.com>
2021-07-22 20:33:58 -0300

    Crie componente principal (App)

---
+++ frontend/src/main/App.jsx
@@ -0,0 +1,18 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'font-awesome/css/font-awesome.min.css'
+import './App.css'
+import React from 'react'
+ 
+import Logo from '../components/template/Logo'
+import Footer from '../components/template/Footer'
+import Main from '../components/template/Main'
+import Nav from '../components/template/Nav'
+ 
+export default props =>
+   <div className="app">
+       <Logo />
+       <Nav />
+       <Main icon="home" title="Início"
+           subtitle="Projeto usando React" />
+       <Footer />
+   </div>
