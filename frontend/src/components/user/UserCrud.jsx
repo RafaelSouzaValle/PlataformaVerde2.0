1671e25ca3211e0f62daa1fccb06e4f454d2c661
Rafael <rafaelsouzavalle@gmail.com>
2021-07-22 23:26:43 -0300

    Create UserCrud.jsx

---
+++ frontend/src/components/user/UserCrud.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+ 
+import Main from '../template/Main'
+ 
+export default class UserCrud extends Component {
+   render() {
+       return (
+           <Main>
+               
+           </Main>
+       )
+ 
+   }
+}
