75207e0ff4b21c7e8f20a0deb28b1827c1b03981
Rafael <rafaelsouzavalle@gmail.com>
2021-07-22 23:10:02 -0300

    Config Main e Adicione topo ao conteúdo

--- frontend/src/components/template/Main.jsx
+++ frontend/src/components/template/Main.jsx
@@ -1,11 +1,19 @@
 import "./Main.css"
 import React from "react"
 import Header from "./Header"
- 
+
 export default props =>
-   <React.Fragment>
-       <Header {...props}/>
-       <main className="content">
-           Conteúdo
-       </main>
-   </React.Fragment>
+    <React.Fragment>
+        <Header />
+        <main className="content container-fluid">
+            <div className="subHeader p-3 mt-3">
+                <div className="icon ub-sub-header"></div>
+                <div className="title ub-sub-header">Cadastro Usuário</div>
+                <div className="obrigatorio ub-sub-header">*campos obrigatórios</div>
+            </div>
+            <hr />
+            <div className="formContent p-3 mt-3">
+                {props.children}
+            </div>
+        </main>
+    </React.Fragment>
